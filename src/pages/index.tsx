14d68ba162642dae1d82a3ff630ea628e4723934
Ramir Mesquita <ramir.mesquita@gmail.com>
2022-06-26 17:07:06 -0300

    Creating sign in page and input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1 >
-      Dashgo
-    </h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input type="email" name="email" label="E-Mail" />
+          <Input type="password" name="password" label="Password" />
+        </Stack>
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+          >
+            Log-in
+          </Button>
+      </Flex>
+    </Flex>
   )
 }
